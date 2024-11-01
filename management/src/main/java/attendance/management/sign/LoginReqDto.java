515dc8d47d1d691a9823cac8b09ee2f7bb868520
suidori <leo7750@naver.com>
2024-11-01 15:23:36 +0900

    1101nyhmerge

--- management/src/main/java/attendance/management/sign/LoginReqDto.java
+++
@@ -1,9 +0,0 @@
-package attendance.management.sign;
-
-import lombok.Data;
-
-@Data
-public class LoginReqDto {
-    String userid;
-    String password;
-}
--- management/src/main/java/attendance/management/userandlecture/UserAndLectureResponseListDto.java
+++
@@ -1,9 +0,0 @@
-package attendance.management.userandlecture;
-
-import lombok.Data;
-
-@Data
-public class UserAndLectureResponseListDto {
-    private Long user;
-    private Long lecture;
-}
