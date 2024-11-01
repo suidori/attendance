048c358dd33a9bbbef8614204ae5923290ae366e
DESKTOP-SCJ83VA\user <leetorrl123@gmail.com>
2024-11-01 15:49:28 +0900

    1101 동현) 병합후 잔오류 수정

--- teamprojectvue/src/App.vue
+++ teamprojectvue/src/App.vue
@@ -1,14 +1,5 @@
 <template>
-<<<<<<< HEAD
-  <nav class="relative mx-0">
-    <div class="flex justify-between">
-      <div class="z-10 flex items-center space-x-4">
-        <div class="w-1/6"><RouterLink to="/">
-          <img src="./images/LOGO.PNG" alt="" class="ml-3  max-md:w-28">
-        </RouterLink></div>
-      </div>
-      <div class="z-10 flex items-center text-white">
-=======
+
   <nav class="mx-0 bg-blue-300 shadow-md">
     <div class="">
       <div class="justify-center flex items-center ">
@@ -17,19 +8,12 @@
         </RouterLink>
       </div>
       <div class="flex text-white z-10 justify-end">
->>>>>>> origin/우혁
         <div class="mr-2"><RouterLink to="/loginview">| 로그인</RouterLink></div>
         <div class="mr-2"><RouterLink to="/jointermsofuse">| 회원가입</RouterLink></div>
         <div class="mr-2"><RouterLink to="/vacationform">| 휴가신청</RouterLink></div>
         <div class="mr-20"><RouterLink to="/laboview">| 연구실</RouterLink></div>
       </div>
-<<<<<<< HEAD
-      <div class="absolute inset-0 h-40 bg-gradient-to-t from-blue-200">
-        <img class="w-full h-40 -z-30" src="../src/images/wbar_t.jpg" alt="" />
-      </div>
-=======
 
->>>>>>> origin/우혁
     </div>
   </nav>
 
--- teamprojectvue/src/router/index.js
+++ teamprojectvue/src/router/index.js
@@ -3,24 +3,16 @@ import LoginView from '../views/guest/LoginView.vue'
 import LoginHome from '@/views/guest/LoginHome.vue'
 import LaboView from '@/views/LaboView.vue'
 import VacationForm from '@/views/student/VacationForm.vue'
-import JoinView from '../views/guest/JoinView.vue'
+import JoinView from '@/views/guest/JoinView.vue'
 import Mypage from '@/views/MyPage.vue'
 import LectureSelect from '@/views/LectureSelect.vue'
 import AttendanceCheck from '@/views/AttendanceCheck.vue'
-<<<<<<< HEAD
 import GuestAttendance from '@/views/guest/GuestAttendance.vue'
-import StudentAttendance from '@/views/student/StudentAttendance.vue'
-import TeacherToday from '@/views/teacher/TeacherToday.vue'
-import TeacherCalander from '@/views/teacher/TeacherCalander.vue'
-import Devmenu from '@/views/dev/Devmenu.vue'
-import DeskCalander from '@/views/Desk/DeskCalander.vue'
-=======
-import GuestAttendance from '@/views/GuestAttendance.vue'
 import JoinTermsOfUse from '@/views/JoinTermsOfUse.vue'
 import LectureApprovallist from '@/views/LectureApprovallist.vue'
 import JoinComplete from '@/views/JoinComplete.vue'
 import LectureInsert from '@/views/LectureInsert.vue'
->>>>>>> origin/우혁
+import StudentAttendance from '@/views/student/StudentAttendance.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -46,15 +38,9 @@ const router = createRouter({
       component: LoginView
     },
     {
-<<<<<<< HEAD
-      path: '/join',
-      name: 'join',
-      component: () => import('../views/guest/JoinView.vue')
-=======
       path: '/joinview',
       name: 'joinview',
-      component: () => import('../views/JoinView.vue')
->>>>>>> origin/우혁
+      component: JoinView
     },
     {
       path: '/laboview',
@@ -87,24 +73,6 @@ const router = createRouter({
       component: AttendanceCheck
     },
     {
-<<<<<<< HEAD
-      path: '/teachertoday',
-      name: 'teachertoday',
-      component: TeacherToday
-    },
-    { path: '/teachercalander',
-      name: 'teachercalander',
-      component: TeacherCalander
-     },
-    { path: '/devmenu',
-      name: 'devmenu',
-      component: Devmenu
-     },
-     {path: '/deskcalander',
-      name: 'deskcalander',
-      component: DeskCalander
-     }
-=======
       path: '/jointermsofuse',
       name: 'jointermsofuse',
       component: JoinTermsOfUse
@@ -124,7 +92,6 @@ const router = createRouter({
       name: 'lectureinsert',
       component: LectureInsert
     },
->>>>>>> origin/우혁
   ]
 })
 
