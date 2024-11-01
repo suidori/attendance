060218544e4ae4fcb49531556783296c3cdfc9ae
DESKTOP-SCJ83VA\user <leetorrl123@gmail.com>
2024-11-01 15:17:27 +0900

    1101

--- management/src/main/java/attendance/management/lecture/LectureRepository.java
+++
@@ -1,9 +0,0 @@
-package attendance.management.lecture;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface LectureRepository extends JpaRepository<Lecture, Long> {
-    Optional<Lecture> findByTitle(String title);
-}
