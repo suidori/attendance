3855c5acc7d9a4cf233a1f808f3f293c4e62902c
DESKTOP-SCJ83VA\user <parkmyounghoi@gmail.com>
2024-11-07 09:52:37 +0900

    241107

--- teamprojectvue/src/api/joinapi.js
+++ teamprojectvue/src/api/joinapi.js
@@ -1,43 +1,34 @@
-import axios from "axios"
-import { useRouter } from "vue-router";
+import axios from 'axios'
+import { useRouter } from 'vue-router'
 
 const url = `http://192.168.103:8080`
 
-   export const checkPhapi = async(phoneNumber) => {
+export const checkPhapi = async (phoneNumber) => {
+  const phonechek = await axios.get(
+    `http://192.168.103:8080/sign/checkphone?phoneNumber=${phoneNumber}`
+  )
 
-            const phonechek = await axios.get(`http://192.168.103:8080/sign/checkphone?phoneNumber=${phoneNumber}`);
-        
-            console.log("폰넘버 후"+phonechek);
+  console.log('폰넘버 후' + phonechek)
 
-      return phonechek
-          
+  return phonechek
 }
-    
 
 export const checkI = async (userid) => {
+  const idResponse = await axios.get(`http://192.168.103:8080/sign/checkid?userid=${userid}`)
 
-        const idResponse = await axios.get(`http://192.168.103:8080/sign/checkid?userid=${userid}`);
-
-    
-      return idResponse
-
+  return idResponse
 }
 
-
 export const joinU = async (data) => {
+  console.log(data)
+  await axios.post(`http://192.168.0.103:8080/sign/signin`, data)
 
-
-      console.log(data)
-     await axios.post(`http://192.168.0.103:8080/sign/signin`, data)
-    
-     if(localStorage.getItem('token')!==null){
-
-      localStorage.removeItem('token')
-     }
-    
-    // } catch (e) {
-    //   console.log(e)
-    //   alert('에러')
-    // }
-
+  if (localStorage.getItem('token') !== null) {
+    localStorage.removeItem('token')
   }
+
+  // } catch (e) {
+  //   console.log(e)
+  //   alert('에러')
+  // }
+}
